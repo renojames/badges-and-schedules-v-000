cbc5876e601d7a75aabfcec3ba07615f4288d53f
Reno Sorensen <renojames19@gmail.com>
2018-02-16 03:32:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
+def batch_badge_creator(attendees)
   badges = []
-  names.each do |name|
+  attendees.each do |name|
     badges.push(badge_maker(name))
   end
 end
