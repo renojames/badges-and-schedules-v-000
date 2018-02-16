1c2cf0ca919a6a4729025419f4aa86e43865d285
Reno Sorensen <renojames19@gmail.com>
2018-02-16 03:44:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,4 +17,5 @@ def assign_rooms(attendees)
   attendess.each do |name|
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{name.each_with_index + 1}")
   end
+  room_assignments
 end
