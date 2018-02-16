43f0466bca64286b9f1e12fc30a66a2157c8050e
Reno Sorensen <renojames19@gmail.com>
2018-02-16 03:24:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,6 @@
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(attendees)
-  attendees.each do |name|
-    "Hello, my name is #{name}"
-  end
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
 end
