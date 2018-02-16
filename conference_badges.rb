26e4965e119f09c5e17b1af38bd4d36233565690
Reno Sorensen <renojames19@gmail.com>
2018-02-16 04:12:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
- puts batch_badge_creator(attendees)
- puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |i|
+    puts i
+  end
 end
