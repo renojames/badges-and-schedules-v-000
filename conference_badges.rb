258fb23df8878aed5ae59012d89ae3ed693d1279
Reno Sorensen <renojames19@gmail.com>
2018-02-16 03:48:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendess.each do |name|
-    room_assignments.push("Hello, #{name}! You'll be assigned to room #{name.each_with_index + 1}")
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{name.index + 1}")
   end
   room_assignments
 end
