27c843f600db78bc2d157ee6e780d1360b93f8f4
Reno Sorensen <renojames19@gmail.com>
2018-02-16 02:20:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,7 @@
 # Write your code here.
 
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def batch_badge_creator(attendees)
   attendees.each do |name|
     "Hello, my name is #{name}"
